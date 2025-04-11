90a3e11259f703e118ac433c84cc5f25b99d008b
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2025-04-11 19:47:36 +0200

    init

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, context) => {
+  if (isProtectedRoute(context)) {
+    await auth.protect();
+  }
+  else if ((await auth()).userId) {
+    return NextResponse.redirect(new URL("/dashboard", context.url));
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
